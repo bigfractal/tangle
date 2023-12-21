2cba93ce37bee578d0135be416b750d2e0452b3d
teefal <teefal@gmail.com>
2023-12-21 08:49:04 -0500

    cucumber running from maven

---
+++ src/test/java/com/bigfractal/tangle/BfCucumber.java
@@ -0,0 +1,11 @@
+package com.bigfractal.tangle;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines( "cucumber" )
+@SelectClasspathResource( "features" )
+public class BfCucumber {
+}
